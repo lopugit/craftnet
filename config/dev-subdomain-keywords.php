52cb290b733cf8eebab4ca1595f0b8617473045b
Derrick Rowell <derrick.rowell@epandcompany.com>
2021-10-01 16:05:07 -0400

    moving the dev subdomain words into their own file, adding 'stg' word

---
+++ config/dev-subdomain-keywords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'acc',
+    'acceptance',
+    'acceptatie',
+    'craftdemo',
+    'dev',
+    'integration',
+    'loc',
+    'local',
+    'preprod',
+    'qa',
+    'sandbox',
+    'stage',
+    'staging',
+    'systest',
+    'test',
+    'testing',
+    'uat',
+    'stg',
+];
