418266a0e2f7e37fc358cc45ecddcbb55ebd7f17
Brad Bell <brad@pixelandtonic.com>
2021-12-06 12:05:00 -0500

    Use correct path

--- .platform/hooks/postdeploy/10_create_symlinks.sh
+++ .platform/hooks/postdeploy/10_create_symlinks.sh
@@ -2,4 +2,4 @@
 
 rm -rf /var/app/current/web/cpresources
 ln -sf /efs/cpresources /var/app/current/web/cpresources
-chown -R webapp:webapp /var/app/current/cpresources
+chown -R webapp:webapp /var/app/current/web/cpresources
