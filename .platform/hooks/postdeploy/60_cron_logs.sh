be6b8b15a288f2f17079ae095c652fe7d27077d8
Brad Bell <brad@pixelandtonic.com>
2021-12-14 20:27:50 -0800

    cds

--- .platform/hooks/postdeploy/60_cron_logs.sh
+++ .platform/hooks/postdeploy/60_cron_logs.sh
@@ -5,11 +5,11 @@ touch /var/app/current/cron/licenses-send-reminders.log
 touch /var/app/current/cron/packages-update-deps.log
 touch /var/app/current/cron/payouts-update.log
 touch /var/app/current/cron/plugins-update-install-counts.log
-chmod 644 /var/app/current/cron/licenses-process-expired-licenses.log
-chmod 644 /var/app/current/cron/licenses-send-reminders.log
-chmod 644 /var/app/current/cron/packages-update-deps.log
-chmod 644 /var/app/current/cron/payouts-update.log
-chmod 644 /var/app/current/cron/plugins-update-install-counts.log
+chmod 777 /var/app/current/cron/licenses-process-expired-licenses.log
+chmod 777 /var/app/current/cron/licenses-send-reminders.log
+chmod 777 /var/app/current/cron/packages-update-deps.log
+chmod 777 /var/app/current/cron/payouts-update.log
+chmod 777 /var/app/current/cron/plugins-update-install-counts.log
 chown webapp:webapp /var/app/current/cron/licenses-process-expired-licenses.log
 chown webapp:webapp /var/app/current/cron/licenses-send-reminders.log
 chown webapp:webapp /var/app/current/cron/packages-update-deps.log
