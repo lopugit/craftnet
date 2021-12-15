0229a96c449f8cd94d328eb7b4033a2c4fd9a9e2
Brad Bell <brad@pixelandtonic.com>
2021-12-14 19:28:57 -0800

    use platform hook

---
+++ .platform/hooks/postdeploy/60_cron_logs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+touch /var/app/current/cron/licenses-process-expired-licenses.log
+touch /var/app/current/cron/licenses-send-reminders.log
+touch /var/app/current/cron/packages-update-deps.log
+touch /var/app/current/cron/payouts-update.log
+touch /var/app/current/cron/plugins-update-install-counts.log
+chmod 644 /var/app/current/cron/licenses-process-expired-licenses.log
+chmod 644 /var/app/current/cron/licenses-send-reminders.log
+chmod 644 /var/app/current/cron/packages-update-deps.log
+chmod 644 /var/app/current/cron/payouts-update.log
+chmod 644 /var/app/current/cron/plugins-update-install-counts.log
+chown webapp:webapp /var/app/current/cron/licenses-process-expired-licenses.log
+chown webapp:webapp /var/app/current/cron/licenses-send-reminders.log
+chown webapp:webapp /var/app/current/cron/packages-update-deps.log
+chown webapp:webapp /var/app/current/cron/payouts-update.log
+chown webapp:webapp /var/app/current/cron/plugins-update-install-counts.log
