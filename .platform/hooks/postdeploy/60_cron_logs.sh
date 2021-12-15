9cb2ac21c68bdf0532182cb45262867b670b9ffc
Brad Bell <brad@pixelandtonic.com>
2021-12-14 20:05:44 -0800

    tet

--- .platform/hooks/postdeploy/60_cron_logs.sh
+++ .platform/hooks/postdeploy/60_cron_logs.sh
@@ -5,13 +5,3 @@ touch /var/app/current/cron/licenses-send-reminders.log
 touch /var/app/current/cron/packages-update-deps.log
 touch /var/app/current/cron/payouts-update.log
 touch /var/app/current/cron/plugins-update-install-counts.log
-chmod 644 /var/app/current/cron/licenses-process-expired-licenses.log
-chmod 644 /var/app/current/cron/licenses-send-reminders.log
-chmod 644 /var/app/current/cron/packages-update-deps.log
-chmod 644 /var/app/current/cron/payouts-update.log
-chmod 644 /var/app/current/cron/plugins-update-install-counts.log
-chown webapp:webapp /var/app/current/cron/licenses-process-expired-licenses.log
-chown webapp:webapp /var/app/current/cron/licenses-send-reminders.log
-chown webapp:webapp /var/app/current/cron/packages-update-deps.log
-chown webapp:webapp /var/app/current/cron/payouts-update.log
-chown webapp:webapp /var/app/current/cron/plugins-update-install-counts.log
